aedaf3eead61e2e3f81a318c3b352e52cc2e10dd
mcgrady <mcgrady911@foxmail.com>
2022-06-21 15:09:48 +0800

    style: code formatter

--- xarch/src/main/java/com/mcgrady/xarch/ext/Internals.kt
+++ xarch/src/main/java/com/mcgrady/xarch/ext/Internals.kt
@@ -21,7 +21,6 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.database.Cursor
-import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
 import android.util.AttributeSet
